eb4d58eb49523207e3478f7c1e8189e2de8f345d
Abhin <abhinchelakkal@gmail.com>
2024-12-27 11:01:34 -0800

    added firebase

--- myscripts.js
+++ myscripts.js
@@ -7,3 +7,16 @@ function myFunction() {
     x.style.display = "none";
   }
 }
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAYsqKfyQOOLaraJfrye2bKaw_-KxtrBb4",
+  authDomain: "cevconfessions-8e1e0.firebaseapp.com",
+  databaseURL: "https://cevconfessions-8e1e0-default-rtdb.firebaseio.com",
+  projectId: "cevconfessions-8e1e0",
+  storageBucket: "cevconfessions-8e1e0.firebasestorage.app",
+  messagingSenderId: "366382082433",
+  appId: "1:366382082433:web:bd7e8505e31d2a43512080"
+};
+
+firebase.initializeApp(firebaseConfig);
+
