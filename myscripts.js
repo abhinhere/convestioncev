711d23b74ded71cf6a2a89d68b48e234f95e7679
Abhin <abhinchelakkal@gmail.com>
2024-12-27 20:18:06 -0800

    new page firebase working properly

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,50 @@
-
-function myFunction() {
-  var x = document.getElementById("message");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-var firebaseConfig = {
-  apiKey: "AIzaSyAYsqKfyQOOLaraJfrye2bKaw_-KxtrBb4",
-  authDomain: "cevconfessions-8e1e0.firebaseapp.com",
-  databaseURL: "https://cevconfessions-8e1e0-default-rtdb.firebaseio.com",
-  projectId: "cevconfessions-8e1e0",
-  storageBucket: "cevconfessions-8e1e0.firebasestorage.app",
-  messagingSenderId: "366382082433",
-  appId: "1:366382082433:web:bd7e8505e31d2a43512080"
+const firebaseConfig = {
+    apiKey: "AIzaSyA7JfEDmsDkc5mgWJju2464Hpjf2J3RS38",
+    authDomain: "contactform-6c5ed.firebaseapp.com",
+    databaseURL: "https://contactform-6c5ed-default-rtdb.firebaseio.com",
+    projectId: "contactform-6c5ed",
+    storageBucket: "contactform-6c5ed.firebasestorage.app",
+    messagingSenderId: "801934617088",
+    appId: "1:801934617088:web:0ab6386802c5de91cac6e5"
 };
 
+// initialize firebase
 firebase.initializeApp(firebaseConfig);
 
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  
+  var msgContent = getElementVal("msgContent");
+
+  saveMessages(msgContent);
+  window.location.href="./done.html";
+  
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (msgContent) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+   
+    msgContent: msgContent,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+
+const content ='This is content to share';
+
+document.getElementById("share").addEventListener("click",()=>{
+  let whatsappUrl = "whatsapp://send?text="+encodeURIComponent(content)
+})
